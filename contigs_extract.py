2b349b7c4f34c5932e1956fd41fe0151c6f90b4a
Mathieu Clément <mcfly1893@gmail.com>
2019-10-14 10:20:54 +0200

    Update contigs_extract.py

--- contigs_extract.py
+++ contigs_extract.py
@@ -3,6 +3,7 @@
 import os
 import subprocess
 import argparse
+from Bio import SeqIO
 
 parser = argparse.ArgumentParser(description="Extract each contig from an assembly fasta file.")
 parser.add_argument("-s", "--strain", help="strain name to be analysed")
@@ -13,32 +14,30 @@ args = parser.parse_args()
 def contig_extract(assembly, folder):
 	print("Folder used: " + folder, flush=True)
 	print("Assembly used: " + assembly, flush=True)
-	
-	contigs_names = subprocess.getoutput("grep '^>' " + folder + assembly)
-	contigs_full_list = contigs_names.split("\n")
-	contigs_list = []
-	
-	for contig_long in contigs_full_list:
-		contig_short = contig_long.split(" ")[0][1:]
-		contigs_list.append(contig_short)
-		os.system("samtools faidx " + folder + assembly + " " + contig_short + " > " + folder + contig_short + ".fa") 
+
+	contigs_list = os.path.join(folder, "contigs_list.txt")
+	assembly_path = os.path.join(folder, assembly)
+
+	with open(contigs_list, "w") as outfile:
+		for contig in SeqIO.parse(assembly_path, "fasta"):
+			contig_path = os.path.join(folder, contig.id + ".fasta")
+			SeqIO.write(contig, contig_path, "fasta")
+			outfile.write(contig.id + "/n")
 
 
 if args.folder == 'Canu':
-	folder_path = "~/1.Assembly/Canu/" + args.strain + "/"
+	folder_path = os.path.join("~/1.Assembly/Canu/", args.strain)
 	assembly_name = args.strain + ".contigs.fasta"
 	contig_extract(assembly_name, folder_path)
-	
+
 elif args.folder == 'Circlator':
-	folder_path = "~/2.Circlator/" + args.strain + "/"
+	folder_path = os.path.join("~/2.Circlator/", args.strain)
 	assembly_name = "06.fixstart.fasta"
 	contig_extract(assembly_name, folder_path)
-	
+
 elif args.folder == 'Pilon':
-	folder_path = "~/3.Polishing/" + args.strain + "/"
+	folder_path = os.path.join("~/3.Polishing/", args.strain)
 	assembly_name = args.strain + ".final.fasta"
 	contig_extract(assembly_name, folder_path)
 else:
-	if not args.folder.endswith("/"):
-		args.folder = args.folder + "/"
 	contig_extract(args.assembly, args.folder)
